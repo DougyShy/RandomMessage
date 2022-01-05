0db6497b3a57831c2b2c9a1ce73a6042e58e6b76
Dug Shybull <doug_scheible@yahoo.com>
2022-01-05 14:37:13 -0600

    TRIAL COMMIT

--- randomMessage.js
+++ randomMessage.js
@@ -1,6 +1,1 @@
-// This program randomly displays a random message to the user when started
-
-const Messages = ["Message One", "Message Two", "Message Three", "Message Four", "Message Five"];
-
-console.log(Messages[0]);
-
+// NEW HERE
